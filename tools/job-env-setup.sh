29b3658d2117398707ddd60e142314c063ce02e2
Karan Vahi <vahi@isi.edu>
2022-08-25 23:40:23 +0000

    regex match on hostname of the worker node to figure out what to do in job env setup

--- tools/job-env-setup.sh
+++ tools/job-env-setup.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-/bin/env
+# uncomment for debugging
+#/bin/env
 
 export PATH=/usr/bin:/bin
 export USER=`whoami`
 
-# initialize modules
-. /etc/profile.d/z01_lmod.sh
+hostname=`hostname -f`
+#hostname="c455-002.stampede2.tacc.utexas.edu"
+stampede_pattern='^[A-Za-z0-9-]+.stampede2.tacc.utexas.edu$'
+if [[ $hostname =~ $stampede_pattern ]]; then
+    echo "Sourcing job environment for Stampede2"
+    # initialize modules
+    . /etc/profile.d/z01_lmod.sh
+    module load tacc-singularity
 
-module load tacc-singularity
+else
+    echo "No specific job environment sourced"
+fi
+
 
